022a3e03cf5639525e4a601a0a0ee156f7d14a63
botinho12 <botivinicios77@gmail.com>
2026-05-26 00:44:15 -0300

    deixando responsivo para mobile

--- obj/Debug/net8.0/CatalogoDeDoces.AssemblyInfo.cs
+++ obj/Debug/net8.0/CatalogoDeDoces.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatalogoDeDoces")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e5bb578cd5d565fe2f81f4d111b391d6cb5223b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e16bf7d40031f1798e826736c85c271d4656990b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatalogoDeDoces")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatalogoDeDoces")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
